e6c1e09b82b8fc758f1351abce95d2e31102c4af
shreshthshukla <74090495+shreshthshukla@users.noreply.github.com>
2021-04-04 15:48:45 +0530

    Add files via upload

--- Ball.js
+++ Ball.js
@@ -1,15 +1,16 @@
 class Ball {
-	constructor(x,y,r){
+	constructor(x,y){
 		var options={
 			restitution:1,
 			friction:1,
 			density:1.2
 			}
-		this.r=r
+		this.height = 80;
+        this.width = 100;
 		this.image=loadImage("AngryBall.png");
-		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		this.body = Bodies.rectangle(x, y, 100, 80, options)
         this.smokeImage = loadImage("smoke.png");
-        // this.trajectory =[];
+        this.trajectory =[];
 		World.add(world, this.body);
 
 	}
@@ -20,18 +21,18 @@ class Ball {
 			fill(255,0,255)
 			imageMode(CENTER);
 			ellipseMode(CENTER)
-			image(this.image, 0,0,this.r*2, this.r*2)
+			image(this.image, 0,0,this.width, this.height)
 			pop()
 
-            // if(this.body.velocity.x > 10 && this.body.position.x > 200){
-            //     var position = [this.body.position.x, this.body.position.y];
-            //     this.trajectory.push(position);
-            //   }
+            if(this.body.velocity.x > 10 && this.body.position.x > 200){
+                var position = [this.body.position.x, this.body.position.y];
+                this.trajectory.push(position);
+              }
              
           
-            //   for(var i=0; i<this.trajectory.length; i++){
-            //     image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
-            //   }
+              for(var i=0; i<this.trajectory.length; i++){
+                image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
+              }
 			
 	}
 
--- sketch.js
+++ sketch.js
@@ -6,11 +6,18 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var ball
 
+function preload(){
+  slingImage = loadImage("Sling.png");
+}
+
 function setup() {
   createCanvas(1500,500);
 
   engine = Engine.create();
+
   world = engine.world;
+
+  Angryball = new Ball(250,280)
 
   ball1 = new Pinkball(950,450);
 
@@ -30,11 +37,15 @@ function setup() {
 
   wood5 = new Wood1(950,220,90,90,10)
 
-  log3 = new Wood2(950,120,20,150, PI/7);
+  // log3 = new Wood2(950,120,20,150, PI/7);
 
-  log4 = new Wood2(870,120,20,150, -PI/7)
+  // log4 = new Wood2(870,120,20,150, -PI/7)
+
+  slingshot = new Rope(Angryball.body,{x:250, y:280});
 
   base = new Ground(1500/2,500,1500,30)
+
+  Engine.run(engine);
 
 }
 
@@ -42,6 +53,8 @@ function draw() {
   background(255,255,255);  
 
   Engine.update(engine);
+
+  image(slingImage,180,240,100,250)
 
   base.display(); 
 
@@ -63,9 +76,20 @@ function draw() {
 
   wood5.display();
 
-  log3.display();
+  // log3.display();
 
-  log4.display();
+  // log4.display();
 
-  text(mouseX+","+mouseY,200,200);
+  slingshot.display()
+
+  Angryball.display()
+
+  text(mouseX+","+mouseY,20,200);
+}
+
+function mouseDragged(){
+  Matter.Body.setPosition(Angryball.body,{x:mouseX,y:mouseY});
+}
+function mouseReleased(){
+  slingshot.fly()
 }
